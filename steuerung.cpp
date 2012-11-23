e5b58f0448d825ab8a90672765979bea85846e6f
FOevermann <foevermann@gmx.de>
2012-11-23 22:09:41 +0100

    GUI und Funktionen geändert

--- steuerung.cpp
+++ steuerung.cpp
@@ -1,6 +1,7 @@
 #include "steuerung.h"
 #include "ui_steuerung.h"
 #include "dialog.h"
+#include <QDebug>
 
 Steuerung::Steuerung(QWidget *parent) :
     QMainWindow(parent),
@@ -25,3 +26,43 @@ void Steuerung::on_actionSchlie_en_triggered()
 {
     close();
 }
+
+void Steuerung::on_actionInfo_triggered()
+{
+
+}
+
+void Steuerung::keyPressEvent(QKeyEvent *event)
+{
+   if (event->key() ==  Qt::Key_W)
+   {
+       qDebug() << "rauf";
+   }
+
+   if (event->key() ==  Qt::Key_S)
+   {
+       qDebug() << "runter";
+   }
+
+   if (event->key() ==  Qt::Key_A)
+   {
+       qDebug() << "links";
+   }
+
+   if (event->key() ==  Qt::Key_D)
+   {
+       qDebug() << "rechts";
+   }
+
+   if (event->key() ==  Qt::Key_H)
+   {
+       qDebug() << "schneller";
+   }
+
+   if (event->key() ==  Qt::Key_L)
+   {
+       qDebug() << "langsamer";
+   }
+}
+
+
--- steuerung.h
+++ steuerung.h
@@ -2,6 +2,7 @@
 #define STEUERUNG_H
 
 #include <QMainWindow>
+#include <QKeyEvent>
 
 namespace Ui {
 class Steuerung;
@@ -14,11 +15,16 @@ class Steuerung : public QMainWindow
 public:
     explicit Steuerung(QWidget *parent = 0);
     ~Steuerung();
-    
+
+
 private slots:
     void on_actionVerbindung_triggered();
 
     void on_actionSchlie_en_triggered();
+
+    void keyPressEvent(QKeyEvent *);
+
+    void on_actionInfo_triggered();
 
 private:
     Ui::Steuerung *ui;
